693737957b0baed0c13499cc7b54fd52b4d2d23e
hzcortex <iamwanghz@gmail.com>
2017-01-22 23:07:09 +0800

    add BigInteger add

--- SwordToOffer/SwordToOffer.java
+++ SwordToOffer/SwordToOffer.java
@@ -23,6 +23,36 @@ public class SwordToOffer {
 
     }
 
+    // 面试12 打印 1 到最大的 n 位数
+    // 用大数加法
+    // TODO 考虑前导零的去除
+    public String add(String input1, String input2) {
+        int i = input1.length() - 1;
+        int j = input2.length() - 1;
+
+        StringBuilder builder = new StringBuilder();
+        int carry = 0, num1, num2;
+        while (i >= 0 || j >= 0) {
+            if (i >= 0)
+                num1 = input1.charAt(i) - '0';
+            else
+                num1 = 0;
+
+            if (j >= 0)
+                num2 = input2.charAt(j) - '0';
+            else
+                num2 = 0;
+
+            int sum = num1 + num2 + carry;
+            carry = sum / 10;
+            builder.append(sum % 10);
+            i--;
+            j--;
+        }
+
+        return builder.reverse().toString();
+    }
+
     public static void main(String[] args) {
         int[][] array = {
                 {1, 2, 3, 4},
